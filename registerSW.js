7aeee844202823e8f1dfc2278a0e4584503e6b26
heaminjo <157016835+heaminjo@users.noreply.github.com>
2026-05-29 16:06:12 +0900

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/holdem-hub/sw.js', { scope: '/holdem-hub/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
